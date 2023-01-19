3642f32211c29fa788f9e0a2ce4775332637fb61
4eSHiR <mr.maximus095@mail.ru>
2023-01-19 22:19:11 +0300

    Git added

--- Git.py
+++ Git.py
@@ -13,3 +13,15 @@ git add #добавляет файл в гит
 git status #Информация о файлах
 git diff #Разница между файлами до совершения commit
 git commit -am ""   #Создание commit
+git commit --amend #Изменение имени commit   [insert - изменяем название комита - ESC - ctrl+c - :wq -> save
+                                                                                                 :qa! -> not save
+git log -p -2 #журнал комитов -разнциа между комитами -кол-во послед комитов (q-выход из журнала)
+git diff hash_1_comit..hash_2_commit - #разница между комитами
+git show commit #что было сделано в комите
+git checkout commit #переключиться на версию файлов при этом комите НЕЛЬЗЯ ОТРЫВАТЬ ГОЛОВУ!
+git checkout commit путь к файлу #переключиться на версию файла! при этом комите
+git reset --hard commit #безвозвратно откатывает файлы до этого комита(можно и вперед)
+git branch #Список веток
+git branch имя ветки #создание новой ветки
+git checkout <branch> #Переключчение на ветку
+git merge <branch> # склеиваем <branch > в master
