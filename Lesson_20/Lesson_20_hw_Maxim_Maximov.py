514d78a63aab63500938cf8b4d9e6c73f360a81a
4eSHiR <mr.maximus095@mail.ru>
2023-01-22 22:59:15 +0300

    Update_Lesson_20_hw_Maxim_Maximov

---
+++ Lesson_20/Lesson_20_hw_Maxim_Maximov.py
@@ -0,0 +1,37 @@
+
+
+#
+# def debug(func):
+#
+#     def wrapper(num_1,num_2):
+#         print(f'The name of func is: {func.__name__}, her first argument is {num_1},'
+#               f'the second is {num_2}, and result is {func(num_1,num_2)}')
+#         print(func(num_1,num_2))
+#     return wrapper
+#
+# @debug
+# def simple_oper(num_1,num_2):
+#     result = num_1 * num_2
+#     return result
+#
+# simple_oper(int(input('Enter first number')),int(input('Enter second number')))
+
+
+
+
+
+# def debug(func):
+#
+#     def wrapper(*args):
+#         print(f'The name of func is: {func.__name__}, her first argument is {args[0]},'
+#               f'the second is {args[1]}, and result is {func(*args)}')
+#         print(func(*args))
+#     return wrapper
+#
+# @debug
+# def simple_oper(*args):
+#     result = args[0] * args[1]
+#     return result
+#
+# simple_oper(int(input('Enter first number')),int(input('Enter second number')))
+
